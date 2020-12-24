ee3ffd4f68fce919cc0cf496e42788d1a9d35bb8
ZengJinRong <jinrong.zeng@outlook.com>
2020-12-24 16:14:19 +0800

    Default Changelist

--- src/main/resources/pipe/pipe.js
+++ src/main/resources/pipe/pipe.js
@@ -1,33 +1,54 @@
 function pipe(...fns) {
   return fns.reduceRight((fn1, fn2) => {
     return function compose(...args) {
-      return fn1(fn2(...args))
+
+      // apply 第二个参数 args 仅接受 [类数组对象]
+      return fn1.call(this, fn2.call(this, ...args))
     }
   })
 }
 
-function prop(name, obj) {
-  return obj[name]
+// function prop(name, obj) {
+//   return obj[name]
+// }
+//
+// function setProp(name, obj, val) {
+//   const o = Object.assign({}, obj)
+//   o[name] = val
+//   return o
+// }
+//
+// function a(x, y) {
+//   return x + y
+// }
+//
+// function b(x) {
+//   return x + 'b'
+// }
+//
+// function c(x) {
+//   return x + 'c'
+// }
+//
+// const ab = pipe(a, b, c)
+//
+// console.log(ab(2, 3))
+
+
+const obj = {
+  name: 'obj',
+  a(x, y) {
+    return `[${this.name}] [a] ${x} ${y}`
+  },
+  b(x) {
+    return `[${this.name}] [b] ${x}`
+  },
 }
 
-function setProp(name, obj, val) {
-  const o = Object.assign({}, obj)
-  o[name] = val
-  return o
+const obj2 = {
+  name: 'obj2',
 }
 
-function a(x, y) {
-  return x + y
-}
+obj2.ab = pipe(obj.a, obj.b)
+console.log(obj2.ab('X', 'Y'))
 
-function b(x) {
-  return x + 'b'
-}
-
-function c(x) {
-  return x + 'c'
-}
-
-const ab = pipe(a, b, c)
-
-console.log(ab(2, 3))
